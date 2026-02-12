4398d2c62051c9a12ec8cc049ebae6c31eafa827
Beast-Bourne <137620684+Beast-Bourne@users.noreply.github.com>
2026-02-12 13:47:01 +0000

    added wendland kernel functions but not used them (commit of a semi-working version first)

--- Assets/NebulaScripts/Shaders/InfluenceKernels.hlsl
+++ Assets/NebulaScripts/Shaders/InfluenceKernels.hlsl
@@ -1,4 +1,5 @@
 const float sigma;
+const float C2Const;
 
 float Influence(float dist, float smoothingRadius)
 {
@@ -40,4 +41,37 @@ float DerivativeInfluence(float dist, float smoothingRadius)
     {
         return 0.0f;
     }
+}
+
+float IntPower(float base, int exponent)
+{
+    float result = 1.0f;
+    for (int i = 0; i < exponent; i++)
+    {
+        result *= base;
+    }
+    return result;
+}
+
+float WendlandC2(float dist, float smoothRad)
+{
+    float DimensionFactor = C2Const / (smoothRad * smoothRad * smoothRad);
+    float q = dist / smoothRad;
+    
+    if (q > 2.0f || q < 0.0f) return 0.0f;
+
+    float term1 = IntPower(1.0f - (q * 0.5f), 4);
+    float term2 = (2.0f * q + 1.0f);
+    
+    return (DimensionFactor * term1 * term2);
+}
+
+float WendlandC2Derivative(float dist, float smoothRad)
+{
+    float DimensionFactor = C2Const / (smoothRad * smoothRad * smoothRad * smoothRad);
+    float q = dist / smoothRad;
+    if (q > 2.0f || q < 0.0f) return 0.0f;
+    
+    float term1 = IntPower(1.0f - (q * 0.5f), 3);
+    return (-5.0f * q * DimensionFactor * term1);
 }
